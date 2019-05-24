b77bf5f0979228ec9a4902e6bbb986523a047a1b
kxkx5150 <senna5150ando@gmail.com>
2019-05-24 13:49:09 +0900

    commit

---
+++ js/init.js
@@ -0,0 +1,7 @@
+var require = {
+  baseUrl: window.location.protocol + "//" + window.location.host + window.location.pathname.split("/").slice(0, -1).join("/"),
+  paths: {
+      ace: "js/ace"
+  },
+  waitSeconds: 30
+};
